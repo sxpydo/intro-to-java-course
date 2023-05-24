1b39b787e750bd2e7b77cda53c47bcb34b45c203
Shavana Peynado <shavanapeynado@YC63DXHWR9.local>
2023-05-24 14:06:16 +0400

    completed exercise 3

--- exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
@@ -3,21 +3,66 @@ package com.cbfacademy;
 public class StringExercises {
 
     public String fromCharacters(char[] characters) {
-        // TODO - Write code that creates a String from the input array of characters
+        // Write code that creates a String from the input array of characters
+        
         throw new RuntimeException("Not implemented");
     }
 
     public Long howMany(String text, Character character) {
-        // TODO - Write code to determine how many of the input ${character} are contained in the input ${text}
+        // Write code to determine how many of the input ${character} are contained in the input ${text}
         throw new RuntimeException("Not implemented");
     }
 
-    public Boolean isPalindrome(String word) {
-        // TODO - Write code to determine whether the input ${word} is a palindrome
-        throw new RuntimeException("Not implemented");
+    public static Boolean isPalindrome(String word) {
+        // Write code to determine whether the input ${word} is a palindrome
+        
+        
+        // Pointers pointing to the beginning
+        // and the end of the string
+        int i = 0, j = word.length() - 1;
+ 
+        // While there are characters to compare
+        while (i < j) {
+ 
+            // If there is a mismatch
+            if (word.charAt(i) != word.charAt(j))
+                return false;
+ 
+            // Increment first pointer and
+            // decrement the other
+            i++;
+            j--;
+        }
+ 
+        // Given string is a palindrome
+        return true;
     }
+        
+
 
     public String getName() {
         return "String Exercises";
     }
+
+
+
+     public static void main(String[] args) {
+        // Input string
+        String str = "geeks";
+
+        //Convert the string to lowercase
+        str = str.toLowerCase();
+        // passing boolean function till holding true
+        if (isPalindrome(str))
+
+            // It is a palindrome
+            System.out.print("Yes");
+        else
+
+            // Not a palindrome
+            System.out.print("No");
+    }
+
 }
+
+
