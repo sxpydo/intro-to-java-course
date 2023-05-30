926b86a71cf1dafd7f865225e06191754f20e864
sxpydo <82041490+sxpydo@users.noreply.github.com>
2023-05-30 12:56:36 +0100

    added files for the other exercises from week 2 and updated HelloWorld file

---
+++ getting-started-with-java/com/cbfacademy/Operators.java
@@ -0,0 +1,21 @@
+package com.cbfacademy;
+
+public class Operators {
+    public static void main(String... args) {
+
+       
+       
+       
+       
+       //UNARY OPERATORS - Increment and Decrement
+    
+            int number1 = 12;
+            int number2 = 12;
+
+            System.out.println(number1++); // 12 - Number was incremented after, therefore you should get 13 on line 16
+            System.out.println(number1); // 13
+            System.out.println(++number2); // 13 - Number was incremented beforehand
+
+    }
+    
+}
---
+++ getting-started-with-java/com/cbfacademy/WrapperArray.java
@@ -0,0 +1,18 @@
+package com.cbfacademy;
+
+public class WrapperArray {
+
+    public static void main(String... args) {
+        int[] int_elements = {7, 4, 9, 3, 007, 266, 21, 45};
+        System.out.println(int_elements.length);
+
+        float[] ft_elements = {10.0f, 7.4f, 23.5f, 0.9f, 5.0f, 9.0f, 10.9f, 2.3f, 1.2f, 0.07f, 45.7f, 5.678f};
+        System.out.println(ft_elements.length);
+
+        //float[] floatArray = new float[5];
+
+
+    
+    }
+    
+}
