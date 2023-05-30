a1a47782dac0671e11d14dc525f7c894d1dc1ea4
sxpydo <82041490+sxpydo@users.noreply.github.com>
2023-05-30 14:05:49 +0100

    completed operators exercises from week 2

--- getting-started-with-java/com/cbfacademy/Operators.java
+++ getting-started-with-java/com/cbfacademy/Operators.java
@@ -3,18 +3,50 @@ package com.cbfacademy;
 public class Operators {
     public static void main(String... args) {
 
-       
-       
-       
-       
-       //UNARY OPERATORS - Increment and Decrement
+        // Arithmetic Operations
+        // Following our example of arithmetic operations on integers, create a simple Java program with simple expressions for both float and double types
+        
+        float number1 = 12.2f;
+        float number2 = 45.5f;
     
-            int number1 = 12;
-            int number2 = 12;
+        // sums for decimal numbers
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiply = number1 * number2;
+        float divide = number1 / number2;
+        
+        // print the results
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiply);
+        System.out.println(divide);
 
-            System.out.println(number1++); // 12 - Number was incremented after, therefore you should get 13 on line 16
-            System.out.println(number1); // 13
-            System.out.println(++number2); // 13 - Number was incremented beforehand
+
+
+       
+        // Relational Operations
+        // Following our example of relational operations on integers, create a simple Java program with simple expressions for doubles only
+        
+        double number3 = 1.2d;
+        double number4 = 4.5d;
+
+        // sums for decimal numbers + print the results
+        System.out.println(number3 == number4);
+        System.out.println(number3 >= number4);
+        System.out.println(number3 <= number4);
+        System.out.println(number3 != number4);
+
+   
+
+       
+       // Unary Operators - Increment and Decrement
+    
+            int number5 = 12;
+            int number6 = 12;
+
+            System.out.println(number5++); // 12 - Number was incremented after, therefore you should get 13 on line 16
+            System.out.println(number5); // 13
+            System.out.println(++number6); // 13 - Number was incremented beforehand
 
     }
     
