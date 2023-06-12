d9ef077ba0bea3d6882e35572a0ce92f3560d6ac
sxpydo <82041490+sxpydo@users.noreply.github.com>
2023-06-12 19:48:08 +0100

    completed the whichMonth exercise

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -7,25 +7,65 @@ import java.util.Map;
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
-        // TODO - Implement this method such that
+        // Implement this method such that
+        
+
         //  it creates a list where for each element of the input list ${numbers}
+        
+    
         //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
+            
+       
         //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
+          
+       
         //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
+             
+      
         //  - it adds the element to the list in any other case
+           
+      
         //  - it returns the constructed list
         throw new RuntimeException("Not implemented");
+
     }
 
     public String whichMonth(Integer number) {
-        // TODO - Implement this method such that
+        // Implement this method such that
+        switch (number) {
         //  - it returns the month corresponding to the input ${number}
+        case 1:
+                return "January";
+            case 2:
+                return "February";  
+            case 3:
+                return "March";                
+            case 4:
+                return "April";
+            case 5:  
+                return "May";
+            case 6:  
+                return "June";
+            case 7:  
+                return "July";
+            case 8:  
+                return "August";
+            case 9:  
+                return "September";
+            case 10:  
+                return "October";
+            case 11:  
+                return "November";
+            case 12:  
+                return "December";
         //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
+        default:
+                return "Invalid month number";
+        }
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
-        // TODO - Implement this method such that
+        // Implement this method such that
         //  - creates and initialises a list of 100 numbers - from 1 to 100
         //  - determines the sum of all the even numbers in the list
         //  - determines the sum of all the odd numbers in the list
@@ -35,9 +75,8 @@ public class FlowControlExercises {
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
-        // TODO - Implement this method such that
+        // Implement this method such that
         //  - it takes an array list of integers
-        //  - it returns the list in reverse order
         throw new RuntimeException("Not implemented");
     }
 
