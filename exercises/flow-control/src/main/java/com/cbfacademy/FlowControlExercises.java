48e5fe27b82b19856f0c257c56ed2980ca5ae858
sxpydo <82041490+sxpydo@users.noreply.github.com>
2023-06-17 19:10:15 +0100

    completed FizzBuzz task, partly solved the car exercise

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,33 +1,33 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FlowControlExercises {
 
-    public List<String> fizzBuzz(List<Integer> numbers) {
+   public List<String> fizzBuzz(List<Integer> numbers) {
         // Implement this method such that
-        
-
+        ArrayList<String> numbersArray = new ArrayList<>();
         //  it creates a list where for each element of the input list ${numbers}
-        
-    
-        //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
-            
-       
-        //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
-          
-       
-        //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
-             
-      
-        //  - it adds the element to the list in any other case
-           
-      
+        for (int i = 0; i < numbers.size(); i++) {
+            //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
+            if ((numbers.get(i) % 3 == 0) && (numbers.get(i) % 5 == 0)) {
+                numbersArray.add("FizzBuzz");
+            //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
+            } else if (numbers.get(i) % 3 == 0) {
+                numbersArray.add("Fizz");
+            //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
+            } else if (numbers.get(i) % 5 == 0) {
+                numbersArray.add("Buzz");
+            //  - it adds the element to the list in any other case
+            } else {
+                numbersArray.add(Integer.toString(numbers.get(i)));
+            }
+        }
         //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
-
+        return numbersArray;
     }
 
     public String whichMonth(Integer number) {
@@ -66,12 +66,31 @@ public class FlowControlExercises {
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
         // Implement this method such that
+        ArrayList<Integer> number = new ArrayList<>();
+
         //  - creates and initialises a list of 100 numbers - from 1 to 100
+            for (int i = 1; i < 100; i++) {
+            number.add(i);
+        }
+
+        HashMap<String, Integer> numberMap = new HashMap<>();
+        numberMap.put("SumOfEvens",0);
+        numberMap.put("SumOfOdds",0);
+
+        for (int i : number) {
         //  - determines the sum of all the even numbers in the list
-        //  - determines the sum of all the odd numbers in the list
+            if (i % 2 == 0) {
+                 numberMap.put("SumOfEvens", numberMap.get("SumOfEvens") + i);
+        
+                 //  - determines the sum of all the odd numbers in the list
+            } else {
+                numberMap.put("SumOfOdds", numberMap.get("SumOfOdds") + i);
+            }
+        }
         //  - returns a map with two entries:
         //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-        throw new RuntimeException("Not implemented");
+        
+        return numberMap;
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
