a9937ef1b5f5f6b0d4872f0719f731d7c27aec6b
sxpydo <82041490+sxpydo@users.noreply.github.com>
2023-06-19 20:17:18 +0100

    completed the Immutablity task

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/people/ImmutablePerson.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/people/ImmutablePerson.java
@@ -1,5 +1,60 @@
 package com.cbfacademy.people;
 
+import java.util.Arrays;
+
+
 public class ImmutablePerson {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String[] nickNames;
+
+    // Using constructor when inputting full name excluding nicknames
+    public ImmutablePerson(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Using constructor when inputting all attributes
+    public ImmutablePerson(String firstName, String lastName, String middleName, String[] nickNames) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.nickNames = nickNames;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+     public void setNickNames(String[] nickNames) {
+        this.nickNames = nickNames;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+     public String getLastName(){
+        return this.lastName;
+    }
+
+     public String getMiddleName(){
+        return this.middleName;
+    }
+
+    public String getNickNames() {
+        return Arrays.toString(this.nickNames);
+    }
+
+
+      public static void main(String[] args) {
+        ImmutablePerson Shav = new ImmutablePerson("Shavana", "Peynado");
+
+        Shav.setMiddleName("Elvereta");
+        Shav.setNickNames(new String[]{"Shavs", "Shavy", "Shav"});
+        System.out.println(Shav.getFirstName());
+        System.out.println(Shav.getNickNames());
+    }
     
 }
